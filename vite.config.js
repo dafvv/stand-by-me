bb93c8159e73a84d0608f720bda9d448dc5d62ef
Daffa Bisma Rudidharma <daffarudidharma@gmail.com>
2026-04-21 19:17:48 +0700

    Set base path and format plugins in vite.config.js
    Updated Vite configuration to set the base path and format plugins array.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-});
+  base: '/stand-by-me/', // HARUS sama persis dengan nama repository GitHub-mu
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
